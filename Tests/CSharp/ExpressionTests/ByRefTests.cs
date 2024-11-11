8257f37fc7b1432ffbe1071fa35086fb3ae41d94
gaschd <gaschd@users.noreply.github.com>
2024-11-11 22:28:39 +0100

    Adding test for #1158 regarding properties used as ref parameter in extension methods

--- Tests/CSharp/ExpressionTests/ByRefTests.cs
+++ Tests/CSharp/ExpressionTests/ByRefTests.cs
@@ -913,4 +913,56 @@ public partial class BinaryExpressionRefParameter
 }");
     }
 
+    [Fact]
+    public async Task ExtensionMethodsRefPropertyParameterAsync()
+    {
+        await TestConversionVisualBasicToCSharpAsync(@"
+Public Class ExtensionMethodsRefPropertyParameter
+    Public Property Number As Integer = 3
+    Public Sub WithExtensionMethod()
+        Number.NegEx()
+    End Sub
+    Public Sub WithMethod()
+        Neg(Number)
+    End Sub
+End Class
+
+Public Module MathEx
+    <Extension()>
+    Public Sub NegEx(ByRef num As Integer)
+        num = -num
+    End Sub
+    Public Sub Neg(ByRef num As Integer)
+        num = -num
+    End Sub
+End Module", @"
+public partial class ExtensionMethodsRefPropertyParameter
+{
+    public int Number { get; set; } = 3;
+    public void WithExtensionMethod()
+    {
+        int argnum = Number;
+        argnum.NegEx();
+        Number = argnum;
+    }
+    public void WithMethod()
+    {
+        int argnum = Number;
+        MathEx.Neg(ref argnum);
+        Number = argnum;
+    }
+}
+              
+public static partial class MathEx
+{
+    public static void NegEx(this ref int num)
+    {
+        num = -num;
+    }    
+    public static void Neg(ref int num)
+    {
+        num = -num;
+    }
+}");
+    }
 }
